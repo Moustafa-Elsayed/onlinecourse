0d9432030e495aa281009ce4890723b8fca507bc
mostafaelsayed <moelsayed949@gmail.com>
2024-07-20 17:06:21 +0300

    work on styling

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -9,16 +9,13 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
 import Logo from "../../../public/Image/Logo.png";
 import Image from "next/image";
 import theme from "@/styles/theme";
 import CustomButton from "../shared/CustomButton";
-import { Margin } from "@mui/icons-material";
 const pages = ["Home", "Courses", "About Us", "Pricing", "Contact"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
+// const settings = ["Profile", "Account", "Dashboard", "Logout"];
 const NavBar = () => {
   const [activeButton, setActiveButton] = React.useState("login");
 
@@ -45,56 +42,18 @@ const NavBar = () => {
   };
 
   return (
-    <AppBar position="static" elevation={0} sx={{ backgroundColor: "white" }}>
+    <AppBar position="static" elevation={0} sx={{ backgroundColor: "transparent" }}>
       <Container
         maxWidth="lg"
         sx={{
-          backgroundColor: "white",
+          backgroundColor: "transparent",
         }}
       >
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
           <Image src={Logo} alt="logo" width={50} height={50} />
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center" sx={{ color: "red" }}>
-                    {page}
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          
+          
           <Typography
             variant="h5"
             noWrap
@@ -111,9 +70,9 @@ const NavBar = () => {
               textDecoration: "none",
             }}
           >
-            LOGO
+            
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+          <Box sx={{ flexGrow: 1, ml: 5, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
                 key={page}
@@ -153,7 +112,7 @@ const NavBar = () => {
               onClick={() => handleButtonClick("login")}
             />
 
-            <Menu
+            {/* <Menu
               sx={{ mt: "45px" }}
               id="menu-appbar"
               anchorEl={anchorElUser}
@@ -172,6 +131,44 @@ const NavBar = () => {
               {settings.map((setting) => (
                 <MenuItem key={setting} onClick={handleCloseUserMenu}>
                   <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu> */}
+          </Box>
+          <Box sx={{  display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="red"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center" sx={{ color: "black" }}>
+                    {page}
+                  </Typography>
                 </MenuItem>
               ))}
             </Menu>
---
+++ src/pages/login/index.js
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const index = () => {
+  return <Box sx={{
+    backgroundColor:"white"
+  }}
+  
+  >login</Box>;
+};
+
+export default index;
