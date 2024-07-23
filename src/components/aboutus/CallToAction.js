046a2969f5589d0c828072f700a3f51663109227
mostafaelsayed <moelsayed949@gmail.com>
2024-07-23 16:27:57 +0300

    finish callaction about us

--- src/components/aboutus/CallToAction.js
+++ src/components/aboutus/CallToAction.js
@@ -5,59 +5,66 @@ import { styled } from "@mui/system";
 import theme from "@/styles/theme";
 import CustomButton from "../shared/CustomButton";
 
-const Root = styled(Box)(({ theme }) => ({
-  padding: theme.spacing(4),
-  textAlign: "center",
-  backgroundColor: "white",
-  display: "flex",
-  alignItems:"center",
-  justifyContent:"center"
-}));
-
-const CTAButton = styled(Button)(({ theme }) => ({
-  marginTop: theme.spacing(2),
-}));
-
 const CallToAction = () => {
   return (
-    <Root>
-      <Box sx={{ flexGrow: 1, textAlign: "left", }}>
-        <Box
-          gutterBottom
-          sx={{
-            fontWeight: "bold",
-            display: "flex",
-            alignItems: "flex-start",
-            maxWidth: "50%",
-          }}
-        >
-          <Typography variant="h2" sx={{ fontWeight: "bold" }}>
-            <Typography
-              variant="h2"
-              component="span"
-              sx={{ color: theme.palette.secondary.main, fontWeight: "bold" }}
-            >
-              Together
+    <Box
+      sx={{
+        textAlign: "center",
+        p: { xs: 1, sm: 1, md: theme.spacing(4) },
+      }}
+    >
+      <Box
+        sx={{
+          backgroundColor: "white",
+          width: "100%",
+          p: { xs: 1, sm: 1, md: theme.spacing(4) },
+          borderRadius: "10px",
+          display: "flex",
+          alignItems: { xs: "flex-start", sm: "flex-start", md: "center" },
+          justifyContent: "center ",
+          flexDirection: { xs: "column", sm: "column", md: "row" },
+        }}
+      >
+        <Box sx={{ flexGrow: 1, textAlign: "left" }}>
+          <Box
+            gutterBottom
+            sx={{
+              fontWeight: "bold",
+              display: "flex",
+              alignItems: "flex-start",
+            }}
+          >
+            <Typography variant="h2" sx={{ fontWeight: "bold" }}>
+              <Typography
+                variant="h2"
+                component="span"
+                sx={{ color: theme.palette.secondary.main, fontWeight: "bold" }}
+              >
+                Together
+              </Typography>
+              , let's shape the future of digital innovation
             </Typography>
-            , let's shape the future of digital innovation
+          </Box>
+          <Typography
+            sx={{ mt: 2, mb: 2 }}
+            variant="body2"
+            gutterBottom
+            color="textSecondary"
+          >
+            Join us in this exciting journey to grow and unlock your potential
+            in design and development.
           </Typography>
         </Box>
-        <Typography variant="body2" gutterBottom color="textSecondary">
-          Join us in this exciting journey to grow and unlock your potential in
-          design and development.
-        </Typography>
+        <Box>
+          <CustomButton
+            backgroundColor={theme.palette.secondary.main}
+            title={" Join Now"}
+            width={"100%"}
+            color={"white"}
+          />
+        </Box>
       </Box>
-<Box>
-
-<CustomButton
-        backgroundColor={theme.palette.secondary.main}
-        title={" Join Now"}
-        width={"100%"}
-        color={"white"}
-      />
-</Box>
-      
-    </Root>
+    </Box>
   );
 };
 
