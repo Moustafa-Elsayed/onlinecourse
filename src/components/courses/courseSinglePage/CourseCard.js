d38e006b6a1f61133655bcfe1223ffcb8fbdb7dc
mostafaelsayed <moelsayed949@gmail.com>
2024-08-01 15:13:50 +0300

    work on singleCourse Page

---
+++ src/components/courses/courseSinglePage/CourseCard.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { Card, CardContent, Typography, Box, Grid } from "@mui/material";
+import theme from "@/styles/theme";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+const CourseCard = ({ number, title, lessons }) => {
+  return (
+    <Card sx={{ borderRadius: 2, backgroundColor: "white" }} elevation={1}>
+      <CardContent>
+        <Typography
+          variant="h4"
+          sx={{
+            fontWeight: "bold",
+            mr: 2,
+            textAlign: "right",
+            fontSize: "40px",
+          }}
+        >
+          {number}
+        </Typography>
+
+        <Typography variant="h4" sx={{ mb: 4, fontWeight: "bold" }}>
+          {title}
+        </Typography>
+        {lessons.map((lesson, index) => (
+          <Box
+            sx={{
+              border: "1px solid #e4e2e2",
+              p: 2,
+              display: "flex",
+              justifyContent: "space-between",
+              flexDirection: { xs: "column", sm: "column", md: "row" },
+              alignItems: { xs: "flex-start", sm: "flex-start", md: "center" },
+              borderRadius: 2,
+              mb: 2,
+              "&:hover": {
+                boxShadow: "1px 0px 8px #ff950070",
+                cursor: "pointer",
+              },
+
+              "&:hover .lesson-duration": {
+                backgroundColor: "#ff950070",
+              },
+              "&:hover .lesson-name": {
+                fontWeight: "bold",
+              },
+            }}
+          >
+            <Box className="lesson-name">
+              <Typography variant="body1" fontWeight={"550"}>
+                {lesson.name}
+              </Typography>
+              <Typography variant="body1" color="textSecondary">
+                lesson {lesson.number}
+              </Typography>
+            </Box>
+            <Typography
+              className="lesson-duration"
+              color="textSecondary"
+              sx={{
+                p: 1,
+                backgroundColor: theme.palette.primary.light,
+                borderRadius: 2,
+                display: "inline-flex",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: 0.5,
+              }}
+            >
+              <AccessTimeIcon /> {lesson.duration}
+            </Typography>
+          </Box>
+        ))}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default CourseCard;
--- src/pages/courses/[courseId].js
+++ src/pages/courses/[courseId].js
@@ -3,8 +3,78 @@ import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 import { fetchCourses } from "@/redux/actions/courses/coursesActions";
 import LoadingSpinner from "@/components/shared/LoadingSpinner";
-
+import PageTitle from "@/components/shared/PageTitle";
+import Grid from "@mui/material/Grid";
+import CourseCard from "@/components/courses/courseSinglePage/CourseCard";
+import Image from "next/image";
+import CourseImage from "../../../public/Image/Container.png";
 const CourseDetail = () => {
+  const coursess = [
+    {
+      number: "01",
+      title: "Introduction to UI/UX Design",
+      lessons: [
+        {
+          name: "Understanding UI/UX Design Principles",
+          number: "01",
+          duration: "45 Minutes",
+        },
+        {
+          name: "Importance of User-Centered Design",
+          duration: "1 Hour",
+          number: "02",
+        },
+        {
+          name: "The Role of UI/UX Design in Product Development",
+          duration: "45 Minutes",
+          number: "03",
+        },
+      ],
+    },
+    {
+      number: "02",
+      title: "Introduction to UI/UX Design",
+      lessons: [
+        {
+          name: "Understanding UI/UX Design Principles",
+          number: "01",
+          duration: "45 Minutes",
+        },
+        {
+          name: "Importance of User-Centered Design",
+          duration: "1 Hour",
+          number: "02",
+        },
+        {
+          name: "The Role of UI/UX Design in Product Development",
+          duration: "45 Minutes",
+          number: "03",
+        },
+      ],
+    },
+    {
+      number: "03",
+      title: "Introduction to UI/UX Design",
+      lessons: [
+        {
+          name: "Understanding UI/UX Design Principles",
+          number: "01",
+          duration: "45 Minutes",
+        },
+        {
+          name: "Importance of User-Centered Design",
+          duration: "1 Hour",
+          number: "02",
+        },
+        {
+          name: "The Role of UI/UX Design in Product Development",
+          duration: "45 Minutes",
+          number: "03",
+        },
+      ],
+    },
+  ];
+
   const router = useRouter();
   const { courseId } = router.query;
 
@@ -15,28 +85,37 @@ const CourseDetail = () => {
 
   useEffect(() => {
     if (!courses) {
-      // Fetch courses if not already fetched
-      dispatch(fetchCourses()); // Replace with your actual fetch action
+      dispatch(fetchCourses());
     }
 
     if (courseId && courses?.length > 0) {
       const selectedCourse = courses.find((course) => course._id === courseId);
-      setCourse(selectedCourse || null); // Set course or null if not found
+      setCourse(selectedCourse || null);
     }
   }, [courseId, courses, dispatch]);
 
-  if (!course)
-    return (
-      <>
-        <LoadingSpinner />
-      </>
-    );
+  if (!course) {
+    return <LoadingSpinner />;
+  }
 
   return (
-    <div>
-      <h1>{course.title}</h1>
-      <p>{course.duration}</p>
-    </div>
+    <>
+      <PageTitle title={course.title} subTitle={course.subtitle} />
+      <Image
+        src={CourseImage} 
+        alt="Course Image"
+        layout="responsive" 
+        width={700} 
+        height={475} 
+      />
+      <Grid container spacing={2} mt={5}>
+        {coursess.map((course, index) => (
+          <Grid item xs={12} sm={6} key={index}>
+            <CourseCard {...course} />
+          </Grid>
+        ))}
+      </Grid>
+    </>
   );
 };
 
