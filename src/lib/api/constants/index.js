4ba4d9bfd31fc14a3de89aef2f7479d3b8695295
mostafaelsayed <moelsayed949@gmail.com>
2024-08-31 21:32:08 +0300

    fixed build issue

--- src/lib/api/constants/index.js
+++ src/lib/api/constants/index.js
@@ -1,9 +1,8 @@
 export const URL_API="https://onlinecourses.up.railway.app/"
-export const BaseUrl = "http://localhost:4000/v1/api";
+// export const BaseUrl = "http://localhost:4000/v1/api";
 // export const BaseUrl = `${URL_API}api`;
-
 export const MainUrl = "http://localhost:4000/";
-// export const BaseUrl = "https://testapi-tau-nine.vercel.app/v1/api";
+export const BaseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
 
 
