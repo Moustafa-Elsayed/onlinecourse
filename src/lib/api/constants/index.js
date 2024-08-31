93d80609a0f451d63d476eb3469ee48ac33419ed
mostafaelsayed <moelsayed949@gmail.com>
2024-08-31 21:41:20 +0300

    fixed issue

--- src/lib/api/constants/index.js
+++ src/lib/api/constants/index.js
@@ -1,7 +1,4 @@
-export const URL_API="https://onlinecourses.up.railway.app/"
-// export const BaseUrl = "http://localhost:4000/v1/api";
-// export const BaseUrl = `${URL_API}api`;
-export const MainUrl = "http://localhost:4000/";
+export const MainUrl = process.env.NEXT_PUBLIC_ma;
 export const BaseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
 
